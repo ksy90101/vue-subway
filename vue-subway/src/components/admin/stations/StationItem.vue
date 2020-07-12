b8b76c57095d9bce7452de5a8c50e071ec2bbc42
SeYun <ksy90101@naver.com>
2020-07-12 14:32:51 +0900

    feat: 역 관리 화면 구성

---
+++ vue-subway/src/components/admin/stations/StationItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="list-item border border-gray-200 py-2 px-4 text-gray-800"
+    v-bind:data-id="station.id"
+  >
+    {{ station.name }}
+    <button
+      class="hover:bg-gray-300 hover:text-gray-500 text-gray-300 px-1 rounded-full float-right"
+    >
+      <span class="mdi mdi-delete"></span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      station: {
+        id: 1,
+        name: "잠실역",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
--- vue-subway/src/components/admin/stations/SubwayStations.vue
+++ vue-subway/src/components/admin/stations/SubwayStations.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
-      역 관리
+  <div class="bg-gray-200 h-screen">
+    <div class="flex justify-center md:py-10 lg:py-10 app-container">
+      <div class="max-w-md width-450px rounded bg-white shadow-lg p-6">
+        <div class="font-bold text-xl mb-3 text-center">지하철역 관리</div>
+        <form>
+          <div class="mb-4">
+            <input
+              class="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+              id="station-name"
+              type="text"
+              autocomplete="off"
+              placeholder="역 이름을 추가해주세요."
+            />
+            <button
+              id="station-add-btn"
+              class="inline-block text-sm bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 font-bold py-2 px-4 rounded"
+            >
+              추가
+            </button>
+          </div>
+        </form>
+        <div class="mt-5 flex station-list-container relative overflow-y-auto">
+          <div id="station-list" class="w-full">
+            <station-item></station-item>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script type="module" src="/admin/js/AdminApp.js"></script>
+    <script type="module" src="/admin/js/views/AdminStation.js"></script>
   </div>
 </template>
 
 <script>
+import stationItem from "./StationItem";
 export default {
-
-}
+  components: {
+    "station-item": stationItem,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
