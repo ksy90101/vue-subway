106bf9ba868fbdfcd022403c48420f9d418ebbbb
SeYun <ksy90101@naver.com>
2020-07-12 21:47:46 +0900

    feat: 구간 관리 모달 구현

--- vue-subway/src/components/admin/edges/StationItem.vue
+++ vue-subway/src/components/admin/edges/StationItem.vue
@@ -15,7 +15,7 @@
 <script>
 export default {
   props: {
-    station: Object,
+    station: String,
   },
 };
 </script>
--- vue-subway/src/components/admin/edges/SubwayEdges.vue
+++ vue-subway/src/components/admin/edges/SubwayEdges.vue
@@ -9,6 +9,7 @@
           <div class="flex-1 text-center">구간 관리</div>
           <div class="flex-1 flex justify-end">
             <button
+              v-on:click.prevent="toggleModal"
               id="subway-line-add-btn"
               class="modal-open bg-transparent bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 text-sm px-3 py-1 rounded"
             >
@@ -58,150 +59,34 @@
         </div>
       </div>
     </div>
-
-    <div
-      class="modal z-10 opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center"
-    >
-      <div
-        class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"
-      ></div>
-      <div
-        class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto"
-      >
-        <div class="modal-content text-left px-8 py-4">
-          <div class="flex justify-center items-center pb-3 pt-3">
-            <p class="text-2xl font-bold ">구간 추가</p>
-          </div>
-          <form class="mb-4">
-            <div class="relative w-full mt-3 mb-6">
-              <label class="block text-gray-700 text-sm font-bold mb-2">
-                노선 선택
-              </label>
-              <div class="relative">
-                <select
-                  id="line-select-options"
-                  class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                >
-                </select>
-                <div
-                  class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-                >
-                  <svg
-                    class="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-            <div class="flex flex-wrap mb-10 w-full">
-              <div class="w-5/12 h-12 text-gray-800">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  이전역
-                </label>
-                <div class="relative">
-                  <select
-                    id="previous-select-options"
-                    class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                  >
-                  </select>
-                  <div
-                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-                  >
-                    <svg
-                      class="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="w-2/12 h-12 text-center text-gray-800 flex justify-center items-center relative top-28px"
-              >
-                <span
-                  class="mdi mdi-arrow-left-right-bold relative bottom-6px text-lg"
-                ></span>
-              </div>
-              <div class="w-5/12 h-12 text-gray-800">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  대상역
-                </label>
-                <select
-                  id="next-station-select-options"
-                  class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                >
-                </select>
-              </div>
-            </div>
-
-            <div class="flex flex-wrap mb-10 w-full">
-              <div class="w-5/12 h-12 text-gray-800">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  거리
-                </label>
-                <input
-                  class="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                  id="distance"
-                  type="number"
-                  value="0"
-                />
-              </div>
-              <div class="w-2/12 h-12 text-gray-800"></div>
-              <div class="w-5/12 h-12 text-gray-800">
-                <label class="block text-gray-700 text-sm font-bold mb-2">
-                  연결 시간
-                </label>
-                <input
-                  class="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
-                  id="duration"
-                  type="number"
-                  value="0"
-                />
-              </div>
-            </div>
-
-            <div class="flex justify-end pt-4">
-              <button
-                class="modal-close px-4 bg-transparent p-3 rounded text-gray-600 hover:bg-gray-100 hover:text-gray-700 mr-2 text-sm"
-              >
-                취소
-              </button>
-              <button
-                type="submit"
-                id="create-edge-button"
-                class="px-4 bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 rounded text-white text-sm"
-              >
-                확인
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <edge-modal v-show="isModal" v-on:toggleModal="toggleModal"></edge-modal>
   </div>
 </template>
 
 <script>
 import vueTinySlider from "vue-tiny-slider";
 import edgeItem from "./EdgeItem";
+import edgeModal from "./EdgeModal";
 
 export default {
+  data: function() {
+    return {
+      isModal: false,
+    };
+  },
   components: {
     "tiny-slider": vueTinySlider,
     "edge-item": edgeItem,
+    "edge-modal": edgeModal,
   },
   computed: {
     lines() {
       return this.$store.getters.getLines;
+    },
+  },
+  methods: {
+    toggleModal: function() {
+      this.isModal = !this.isModal;
     },
   },
 };
